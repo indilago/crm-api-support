8737699473a5e0cd3baee9a7ab1838e1c7a0dfc5
Kelly Banman <kelly.banman@gmail.com>
2022-02-19 20:35:37 -0800

    v0.0.8

--- src/customers/submission.ts
+++ src/customers/submission.ts
@@ -1,10 +1,13 @@
+import {ILead} from '../leads'
 
+// @deprecated - use ILeadSubmissionCreateInput
 export interface ILeadSubmissionInput {
   collectionDate: string
   city: string
   province?: string
   notes?: string
 }
+export type ILeadSubmissionCreateInput = ILeadSubmissionInput
 
 export interface ILeadInput {
   name: string
@@ -39,4 +42,20 @@ export interface IListSubmissionsResponse {
 
 export interface ILeadSubmissionResponse {
   submission: ISubmission
+}
+
+// Expose only the attributes we accept as input
+export interface ISubmissionLead {
+  name: string
+  address?: Omit<ILead['primaryAddress'], 'id'>
+  phone?: string
+  email?: string
+}
+
+export interface ISubmissionLeadsResponse {
+  leads: ISubmissionLead[]
+}
+
+export interface ISubmissionAddLeadResponse {
+  lead: ISubmissionLead
 }
